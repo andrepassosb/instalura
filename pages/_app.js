1cbacad62f6de1a9465deffa925041a15e04f1f0
unknown <andre.passosb@gmail.com>
2021-02-20 15:39:04 -0300

    Add Meta Tag

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,11 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Instalura - Projeto Base" />
+        <meta property="og:description" content="Bootcamp JamStack Alura" />
+        <meta property="og:image" content="/images/meta_tag.png" />
+
         <title>Instalura - Projeto Base</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
